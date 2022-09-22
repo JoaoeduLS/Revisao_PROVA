24e3324911b83f472da124dfd1231315c7edb397
unknown <21114290013@ACADEMICO.IESB.BR>
2022-09-21 21:51:06 -0300

    oi

--- app/Controllers/Http/RevisaoProvaController.ts
+++ app/Controllers/Http/RevisaoProvaController.ts
@@ -18,8 +18,67 @@ export default class RevisaoProvaController {
             return{valor_secundario}
 
         }
+    }
+    ex2({request}){
+            const dados = request.body();
+            const maior=Math.max(dados.num1, dados.num2 , dados.num3)
+             
+        return{maior}
+    }
+    ex3({request}){
+        const dados =request.body();
+        const valchopp = 4.80;
+        const valpizza = 17.00;
+        const valadicional = 1.50;
+        const garçom= 1.1
+
+        //calculo
+        const choop= valchopp* dados.choop
+        const pizza = valpizza* dados.pizza
+        const adicional= dados.adicional* valadicional
+        const resultado=  (choop + pizza+ adicional) *garçom
+        let resultado2 = resultado /dados.pessoas 
+
+        const resposta = {
+            resultado: resultado2.toFixed(2)
+        } 
+
+        return resposta
+    }
+    ex4({request}){
+        const dados = request.body()
+        const Htrabalhada= 1/5
+        const dependente =35
+
+        const ponto1= Htrabalhada * dados.salariomin
+        const ponto2= dados.horastrab* ponto1
+        const ponto4= ponto2 / 1.5
+        const ponto5 = ponto1 + ponto4 +dependente
+
+let imposto
+if ( ponto5<2000) {
+    imposto=0
+}
+else if(ponto5<=5000){
+    imposto= ponto5* 0.1
+}
+else{
+    imposto= 0.2* ponto5
+}
+
+        const salarioliq= ponto5- imposto
+
+let gratificaçao
+if (salarioliq <= 3500){
+    gratificaçao= salarioliq+ 1000
+}
+else{
+    gratificaçao= salarioliq+ 500
+    
+}        
+    }
 
     }
-}
+
 
 
--- start/routes.ts
+++ start/routes.ts
@@ -18,8 +18,21 @@
 |
 */
 
+
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/', async () => {
   return { hello: 'world' }
 })
+Route.get('/test', async () => {
+  return { test: 'test' }
+})
+Route.post('/lista2/ex1',"RevisaoProvaController.ex1")
+Route.post('/lista2/ex2',"RevisaoProvaController.ex2")
+Route.post('/lista2/ex3',"RevisaoProvaController.ex3")
+Route.post('/lista2/ex4',"RevisaoProvaController.ex4")
+Route.post('/lista2/ex5',"RevisaoProvaController.ex5")
+Route.post('/lista2/ex6',"RevisaoProvaController.ex6")
+Route.post('/lista2/ex7',"RevisaoProvaController.ex7")
+Route.post('/lista2/ex8',"RevisaoProvaController.ex8")
+Route.post('/lista2/ex9',"RevisaoProvaController.ex9")
